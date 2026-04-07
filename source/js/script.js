d5cc93941daed2f329a0b75cc98d328bcb5a9b6a
alexxx233578 <alybelly1202@gmail.com>
2026-04-06 18:00:57 -0700

    Update script.js

--- source/js/script.js
+++ source/js/script.js
@@ -13,3 +13,37 @@ class Timer {
         }, 1000);
     }
 } // Working on timer rn ^ Hayden
+
+
+// End quiz
+function endQuiz() {
+  clearInterval(timer);
+  document.getElementById("quiz-box").classList.add("hidden");
+  nextBtn.classList.add("hidden");
+
+  resultEl.classList.remove("hidden");
+  resultEl.innerHTML = `
+    <h2>Your Score: ${score}/${questions.length}</h2>
+  `;
+
+  saveScore(score);
+  loadBestScore();
+}
+
+// Local Storage
+function saveScore(score) {
+  let best = localStorage.getItem("bestScore") || 0;
+  if (score > best) {
+    localStorage.setItem("bestScore", score);
+  }
+}
+function loadBestScore() {
+  let best = localStorage.getItem("bestScore") || 0;
+  bestScoreEl.textContent = best;
+}
+
+// Load best score on start
+loadBestScore();
+
+
+//Alex still working on score calculation and local storage^^^
